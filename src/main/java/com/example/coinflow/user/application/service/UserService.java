c67702c28312f5489cbbb4775500756a322a6880
= <gabriel.cirilo.rosa@gmail.com>
2024-10-30 13:47:42 -0300

    [postUser] Implements Service Layer

--- src/main/java/com/example/coinflow/user/application/service/UserService.java
+++ src/main/java/com/example/coinflow/user/application/service/UserService.java
@@ -1,2 +1,8 @@
-package com.example.coinflow.user.application.service;public interface UserService {
+package com.example.coinflow.user.application.service;
+
+import com.example.coinflow.user.application.api.UserRequest;
+import com.example.coinflow.user.application.api.UserResponse;
+
+public interface UserService {
+    UserResponse createUser(UserRequest userRequest);
 }
--- src/main/java/com/example/coinflow/user/application/service/UserServiceApplication.java
+++ src/main/java/com/example/coinflow/user/application/service/UserServiceApplication.java
@@ -1,4 +1,18 @@
 package com.example.coinflow.user.application.service;
 
-public class UserApplicationService {
+import com.example.coinflow.user.application.api.UserRequest;
+import com.example.coinflow.user.application.api.UserResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class UserServiceApplication implements UserService{
+
+    @Override
+    public UserResponse createUser(UserRequest userRequest) {
+        log.info("[Start] UserServiceApplication - createUser");
+        log.info("[Finish] UserServiceApplication - createUser");
+        return null;
+    }
 }
