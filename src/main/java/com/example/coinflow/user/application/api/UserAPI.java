cb4a4bf46934f9b687cbf3aa65ed2d0126818bd1
= <gabriel.cirilo.rosa@gmail.com>
2024-10-30 13:46:17 -0300

    [postUser] Implements API and Controller Layers

--- src/main/java/com/example/coinflow/user/application/api/UserAPI.java
+++ src/main/java/com/example/coinflow/user/application/api/UserAPI.java
@@ -1,2 +1,13 @@
-package com.example.coinflow.user.application.api;public interface UserAPI {
+package com.example.coinflow.user.application.api;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/user")
+public interface UserAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    UserResponse postUser(@Valid @RequestBody UserRequest userRequest);
 }
--- src/main/java/com/example/coinflow/user/application/api/UserController.java
+++ src/main/java/com/example/coinflow/user/application/api/UserController.java
@@ -1,2 +1,21 @@
-package com.example.coinflow.user.application.api;public class UserController {
+package com.example.coinflow.user.application.api;
+
+import com.example.coinflow.user.application.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class UserController implements UserAPI{
+    private final UserService userService;
+
+    @Override
+    public UserResponse postUser(UserRequest userRequest) {
+        log.info("[Start] UserController - postUser");
+        UserResponse userCreated = userService.createUser(userRequest);
+        log.info("[Finish] UserController - postUser");
+        return userCreated;
+    }
 }
